0e5e76e65c9b32cb9f92eaf51f86652784d49a23
DJelgaspi <gm.gimenez@alumno.etec.um.edu.ar>
2025-09-05 10:49:59 -0300

    backup

---
+++ app/src/main/kotlin/org/example/Post.kt
@@ -0,0 +1,11 @@
+package org.example
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Post(
+    val userId: Int,
+    val id: Int,
+    val title: String,
+    val body: String
+)
